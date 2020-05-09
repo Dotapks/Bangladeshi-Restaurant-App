e1a859f28822e019f36830d53bb11c64d3f55c2a
namahin <mahin0w1@gmail.com>
2020-05-09 18:12:02 +0600

    safe project

---
+++ app/src/main/java/com/pyfellow/bangladeshirestaurantapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,25 @@
+package com.pyfellow.bangladeshirestaurantapp.ui.gallery
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProviders
+import com.pyfellow.bangladeshirestaurantapp.R
+
+class GalleryFragment : Fragment() {
+
+    private lateinit var galleryViewModel: GalleryViewModel
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        galleryViewModel =
+                ViewModelProviders.of(this).get(GalleryViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_chat, container, false)
+        return root
+    }
+}
